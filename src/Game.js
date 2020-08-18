296261c4d88507e43d220460d3e803628b6ded0e
Hanna Kim <hannakim91@gmail.com>
2020-08-18 17:02:48 -0600

    Test and add Game.startNewRound() to update Game.currentRound #14

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,16 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('./Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null
+  }
+
+  startNewRound(deck) {
+    this.currentRound = new Round(deck)
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
