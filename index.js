06ee767f10662fbc8da40fad0ce66925e2cb143a
Hanna Kim <hannakim91@gmail.com>
2020-08-18 17:32:54 -0600

    Let Game start with stock data or dataset argument; invoke Game.start in index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const http = require('http');
 let app = http.createServer();
 
+const data = require('./src/data');
+const prototypeQuestions = data.prototypeData;
+const Card = require('./src/Card');
+const Deck = require('./src/Deck');
+const Turn = require('./src/Turn');
+const Round = require('./src/Round');
+const Game = require('./src/Game');
+
+const game = new Game()
+game.start(prototypeQuestions)
+
 // Start the server on port 3000
 app.listen(3000, '127.0.0.1');  
 console.log('Node server running on port 3000'); 
+
--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,8 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 const Round = require('./Round');
-const Card = require('../src/Card');
-const Deck = require('../src/Deck');
+const Card = require('./Card');
+const Deck = require('./Deck');
 
 class Game {
   constructor() {
@@ -14,12 +14,12 @@ class Game {
     this.currentRound = new Round(deck)
   }
 
-  start() {
+  start(cardData) {
     const cardA = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const cardB = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
     const cardC = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
     const cards = [cardA, cardB, cardC]
-    const newDeck = new Deck(cards)
+    const newDeck = new Deck(cardData || cards)
     const newRound = new Round(newDeck)
     this.printMessage(newDeck, newRound)
     this.printQuestion(newRound)
